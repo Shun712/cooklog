cd054d69237aaab10559d556f67461f44cc5f765
sh705vi@gmail.com <sh705vi@gmail.com>
2020-12-21 21:50:10 +0900

    gemのインストールと各種設定

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,13 @@ module Cooklog
       g.test_framework false  # testファイルは生成しない
     end
 
+    g.test_framework :rspec,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
