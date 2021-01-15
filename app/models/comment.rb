d20d16296e71a743b88b7c4e544c6cae9790608f
sh705vi@gmail.com <sh705vi@gmail.com>
2021-01-15 23:53:00 +0900

    Revert "Commentモデルの作成"
    This reverts commit 7ac1e5a4e6f7da26080356bc1ef3595e306bad4d.

--- app/models/comment.rb
+++
@@ -1,6 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :dish
-  validates :user_id, presence: true
-  validates :dish_id, presence: true
-  validates :content, presence: true, length: { maximum: 50 }
-end
