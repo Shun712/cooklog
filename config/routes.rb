ed90c8324a503609ed4af7f9a80808746de1c354
sh705vi@gmail.com <sh705vi@gmail.com>
2021-01-16 07:33:24 +0900

    通知を生成する

--- config/routes.rb
+++ config/routes.rb
@@ -20,11 +20,6 @@ Rails.application.routes.draw do
   get :favorites, to: 'favorites#index'
   post   "favorites/:dish_id/create"  => "favorites#create"
   delete "favorites/:dish_id/destroy" => "favorites#destroy"
-<<<<<<< HEAD
   resources :comments, only: [:create, :destroy]
   resources :notifications, only: :index
-=======
-  resources :notifications, only: :index
-  resources :comments, only: [:create, :destroy]
->>>>>>> origin/master
 end
