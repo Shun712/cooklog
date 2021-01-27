4a72988711d0c2fc830114fcbf3343593fe5730f
sh705vi@gmail.com <sh705vi@gmail.com>
2021-01-28 04:02:42 +0900

    Revert "材料（Ingredient）モデルの作成"
    This reverts commit 88c81ab5e20d904e496f303d2dabb72e119b5a02.

--- spec/factories/ingredients.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :ingredient do
-    name { "小麦粉" }
-    quantity { "100g" }
-    association :dish
-  end
-end
--- spec/models/ingredient_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Ingredient, type: :model do
-  let!(:ingredient) { create(:ingredient) }
-
-  it "有効な状態であること" do
-    expect(ingredient).to be_valid
-  end
-end
