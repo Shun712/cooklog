3fb5511d4a33ac88a52244b3a9fdfd36e75171e9
sh705vi@gmail.com <sh705vi@gmail.com>
2021-01-28 05:16:23 +0900

    材料の登録

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -4,6 +4,7 @@ class DishesController < ApplicationController
   
   def new
     @dish = Dish.new
+    @dish.ingredients.build
   end
 
   def index
@@ -55,10 +56,11 @@ class DishesController < ApplicationController
 
   private
 
-    def dish_params
-      params.require(:dish).permit(:name, :discription, :portion, :tips,
-                                   :reference, :required_time, :popularity, :cook_memo, :picture)
-    end
+  def dish_params
+    params.require(:dish).permit(:name, :description, :portion, :tips,
+                                 :reference, :required_time, :popularity, :cook_memo, :picture,
+                                 ingredients_attributes: [:id, :name, :quantity])
+  end
 
     def correct_user
       # 現在のユーザーが更新対象の料理を保有しているかどうか確認
